c4c4c51dd35e4b4828719f94ec76404f08066014
noViceMin <admin@isamin.kr>
2023-06-19 22:54:27 +0900

    main page update

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res, next) => {
-  res.render('index', {});
+  res.render('index', {
+    domains: JSON.parse(fs.readFileSync('./data/domains.json', 'utf8'))
+  });
 });
 
 app.use('/u', express.static(path.join(__dirname, 'upload')));
